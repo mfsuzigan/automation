9a5f12c5984c9bbdf9298f89d8cbd237220d37f9
mfsuzigan <mfsuzigan@gmail.com>
2023-11-10 07:35:52 -0300

    fix: user folder in args for counter

--- bash/random_image_slideshow.sh
+++ bash/random_image_slideshow.sh
@@ -7,7 +7,8 @@
 
 image_dir=$1
 sleep_in_seconds=$2
-file_count_in_folder=$(find . -maxdepth 1 -type f -regex ".*\.[(jJpP(eE)+gG)|(pPnNgG)|(wWeEbB)]+$" | wc -l)
+file_count_in_folder=$(find $image_dir -maxdepth 1 -type f -regex ".*\.[(jJpP(eE)+gG)|(pPnNgG)|(wWeEbB)]+$" | wc -l)
+echo $file_count_in_folder
 
 while [ 1 == 1 ]; do
   random_file_index=$(( $RANDOM%${file_count_in_folder}+1 ))
